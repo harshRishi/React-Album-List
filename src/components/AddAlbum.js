2f6ed62fc5b8c403bd7ce746fb6d2cbc884b0dda
Harsh <harshuniverse000@gmail.com>
2022-12-22 16:10:40 +0530

    Added different component files

---
+++ src/components/AddAlbum.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AddAlbum() {
+  return (
+    <div>
+      <h1>AddAlbum</h1>
+    </div>
+  );
+}
+
+export default AddAlbum;
---
+++ src/components/UpdateAlbum.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function UpdateAlbum() {
+  return (
+    <div>
+      <h1>UpdateAlbum</h1>
+    </div>
+  );
+}
+
+export default UpdateAlbum;
