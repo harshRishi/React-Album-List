5f739a5fd0185f3d80335c0fe60c118ef15dc291
Harsh <harshuniverse000@gmail.com>
2022-12-22 16:43:50 +0530

    Data fetched from API

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,36 @@
+// Package Imports
+import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
-
+// file imports
 import Navbar from "./Navbar";
-import Home from "./Home";
-import AddAlbum from "./AddAlbum";
-import UpdateAlbum from "./UpdateAlbum";
-import FoF from "./FoF";
+// Pages
+import Home from "../pages/Home";
+import AddAlbum from "../pages/AddAlbum";
+import UpdateAlbum from "../pages/UpdateAlbum";
+import FoF from "../pages/FoF";
+// URL to fetch data from the API
+import { API_URLS } from "../utils/constants";
 
 function App() {
+  // Our app will maintain the posts and albm state
+  const [posts, setPosts] = useState([]);
+  const [album, setAlbum] = useState({});
+
+  // using useEffect we'll fetch the data from the API
+  const fetchData = async () => {
+    const url = API_URLS.getURL();
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setPosts(data);
+      });
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // console.log(posts);
+
   return (
     <div className="App">
       <Navbar />
---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+const ROOT_URL = "https://jsonplaceholder.typicode.com/albums";
+
+export const API_URLS = {
+  getURL: () => {
+    return ROOT_URL;
+  },
+};
