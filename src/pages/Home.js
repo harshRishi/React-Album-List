673513ea698e356ba98587a9ab1e831a2bc269d3
Harsh <harshuniverse000@gmail.com>
2022-12-22 16:51:14 +0530

    Posts added to Home page with basic styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,26 @@
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 
 function Home({ posts }) {
   return (
     <div className="container">
-      <h1>Home</h1>
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        {posts.map((post) => (
+          <div className="card m-2" key={post.id} style={{ width: "17rem" }}>
+            <div
+              className="card-body list"
+              style={{ display: "flex", flexDirection: "column" }}
+            >
+              {/* Title  */}
+              <h5 className="card-title">{post.title}</h5>
+              <div className="row">
+                {/* Update post button */}
+                {/* Delete post button */}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
